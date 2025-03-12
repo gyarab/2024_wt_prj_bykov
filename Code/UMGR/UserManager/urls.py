7bcda35b177bc996b0279786e6162822b97fe8ae
MegapolisPlayer <megapolispl@outlook.com>
2025-03-12 16:21:28 +0100

    rewrote django stuff to NPWS base

--- Code/UMGR/UserManager/urls.py
+++ Code/UMGR/UserManager/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('one', TemplateView.as_view(template_name='main/one.html')),
-    path('two', TemplateView.as_view(template_name='main/two.html')),
+    path('', TemplateView.as_view(template_name='main/index.html')),
+    path('results', TemplateView.as_view(template_name='main/results.html')),
+	path('user', TemplateView.as_view(template_name='main/user.html')),
 ]
